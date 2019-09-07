0d799b38ea9f0d3967643e5b8660e6a2e3023d7b
mattdocherty314 <mattdocherty314@gmail.com>
2019-09-07 17:34:40 +1000

    Made Dynamic Size for Park Query

--- get-park-data.js
+++ get-park-data.js
@@ -1,4 +1,4 @@
-async function getParkData(latlngData) {
+async function getParkData(latlngData, numParks=2) {
     let latN = latlngData.latitude.min;
     let latX = latlngData.latitude.max;
     let lngN = latlngData.longitude.min;
@@ -15,37 +15,27 @@ async function getParkData(latlngData) {
     })
     .then(async function(data) {
         console.log(data);
-        let data1_info = data.features[0].attributes;
-        let data2_info = data.features[1].attributes;
-        
-        let addr1 = `${data1_info.HOUSE_NUMBER} ${data1_info.STREET_ADDRESS}, ${data1_info.SUBURB} ${data1_info.POST_CODE}`;
-        let addr2 = `${data2_info.HOUSE_NUMBER} ${data2_info.STREET_ADDRESS}, ${data2_info.SUBURB} ${data2_info.POST_CODE}`;
+        let outputData = [];
 
-        let latlng1 = {
-            "latitude": data1_info.LAT,
-            "longitude": data1_info.LONG
-        }
-        let latlng2 = {
-            "latitude": data2_info.LAT,
-            "longitude": data2_info.LONG
-        }
-
-        let outputData = [
-            {
-                "name": data1_info.PARK_NAME,
-                "lat": data1_info.LAT,
-                "lng": data1_info.LONG,
-                "address": addr1,
-                "distance": Math.round(getDistanceBetweenLatLngs(latlng, latlng1)*100)/100
-            },
-            {
-                "name": data2_info.PARK_NAME,
-                "lat": data2_info.LAT,
-                "lng": data2_info.LONG,
-                "address": addr2,
-                "distance": Math.round(getDistanceBetweenLatLngs(latlng, latlng2)*100)/100
+        for (let n = 0; n < numParks; n++) {
+            let data_info = data.features[n].attributes;
+            let addr = `${data_info.HOUSE_NUMBER} ${data_info.STREET_ADDRESS}, ${data_info.SUBURB} ${data_info.POST_CODE}`;
+            let latlngP = {
+                "latitude": data_info.LAT,
+                "longitude": data_info.LONG
             }
-        ]
+            let dist = Math.round(getDistanceBetweenLatLngs(latlng, latlngP)*100)/100;
+
+            let outputEntry = {
+                "name": data_info.PARK_NAME,
+                "lat": data_info.LAT,
+                "lng": data_info.LONG,
+                "address": addr,
+                "distance": dist
+            }
+            outputData.push(outputEntry);
+        }
+
         return outputData;
     });
 }
